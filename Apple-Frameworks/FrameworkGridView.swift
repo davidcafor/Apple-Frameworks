421fd58af14b0f09603b8874e3cb4fb2ff9281f6
davidcafor <davidcafor@gmail.com>
2024-04-15 19:12:31 +0200

    FrameworksView

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,52 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by David Cardeña on 15/4/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+        .preferredColorScheme(.dark)
+}
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
