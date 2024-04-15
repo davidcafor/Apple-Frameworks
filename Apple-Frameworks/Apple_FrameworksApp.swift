3a7f85d733d8c2c32f9b18d2bc04e904865279b8
davidcafor <davidcafor@gmail.com>
2024-04-15 18:13:26 +0200

    First commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by David Cardeña on 15/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
